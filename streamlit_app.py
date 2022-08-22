042072cb996ffca772e0180b89cad8bbbe6a9b12
hackthecrisis21 <63074184+hackthecrisis21@users.noreply.github.com>
2022-08-22 00:30:13 -0700

    Update streamlit_app.py
    Made a few more changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
+
+streamlit.header("Breakfast MEnu")
+streamlist.text("Omega 3 & Blueberry Oatmeal")
+streamlist.text('Kale, SPinach, & Rocket Smoothie')
+streamlist.text('Hard-bolied Free-Range Egg')
